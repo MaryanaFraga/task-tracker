f9671abec220cdabe97b151ee4be8133fbd0759b
Maryana Fraga <fraga.azul.maryana@gmail.com>
2025-07-10 19:38:18 -0300

    feat: create Task class

--- src/main.py
+++ src/main.py
@@ -1,9 +1,23 @@
+class Task:
+    # Task's constructor
+    def __init__(self, id, name, status, deadline):
+        self.id = id
+        self.name = name
+        self.status = status
+        self.deadline = deadline
+    
+    # Formatting the Task's object when used as a string
+    def __str__(self):
+        return f'{self.id} - {self.name} | {self.status} | {self.deadline}'
+
+
 def menu():
     print("*** TASK MANAGER ***\n\n"
     "1 - CREATE NEW TASK\n"
     "2 - LIST TASKS\n"
     "3 - DELETE TAKS\n"
     "4 - EXIT\n")
+    
 
 if __name__ == "__main__":
     while(True):
